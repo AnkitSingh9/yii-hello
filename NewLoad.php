eac602969973105a0b5f27a92eab7570da4c3d7e
Developer <ankitsinghchauhan@cedcoss.com>
2015-06-02 16:52:45 +0530

    Files Added

---
+++ NewLoad.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace cedcoss\yii2hello;
+
+/**
+ * This is just an example.
+ */
+class NewLoad extends \yii\base\Widget
+{
+    public function run()
+    {
+        return "This is updated version of Hello Extension...";
+    }
+}
